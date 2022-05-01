9c8ef2fe9bc5de23282d9ffc8d0e8823ba43f5d0
Le Quoc Khanh <khanhlq21011999@gmail.com>
2022-05-01 22:25:59 +0700

    update ios

--- ios/Pods/Target Support Files/Pods-privNote-privNoteTests/Pods-privNote-privNoteTests-frameworks.sh
+++ ios/Pods/Target Support Files/Pods-privNote-privNoteTests/Pods-privNote-privNoteTests-frameworks.sh
@@ -178,6 +178,10 @@ code_sign_if_enabled() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/Flipper-DoubleConversion/double-conversion.framework"
   install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/OpenSSL-Universal/OpenSSL.framework"
+  install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/hermes-engine/hermes.framework"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/hermes-engine/hermes.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
